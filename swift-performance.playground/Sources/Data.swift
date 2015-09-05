af4a8568fd88a5b68e044375e90019fc2145b111
Eugene Tiutiunnyk <ievgen.tiutiunnyk@gmail.com>
2015-09-05 11:06:58 -0700

    Swift 2.0 stride() syntax updated

--- swift-performance.playground/Sources/Data.swift
+++ swift-performance.playground/Sources/Data.swift
@@ -3,7 +3,7 @@ import Foundation
 public func createTestArray(amount:Double)->[String]{
     var data = [String]()
     
-    for _ in stride(from: 0, through: amount, by: 1) {
+    for _ in 0.stride(through: amount, by: 1) {
         data.append(NSUUID().UUIDString)
     }
     
